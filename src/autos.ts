2448952fc2b339689b94ed5dc05ec975d9503a70
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-10 10:39:56 -0300

    inicio

---
+++ src/autos.ts
@@ -0,0 +1,13 @@
+let btnEnv = document.getElementById("btnEnviar");
+let vuelta1 = document.getElementById("vuelta1");
+let vuelta2 = document.getElementById("vuelta2");
+let vuelta3 = document.getElementById("vuelta3");
+let vuelta4 = document.getElementById("vuelta4");
+
+btnEnv.addEventListener("click", () => {
+  let vuelta11: number = Number(vuelta1.value);
+  let vuelta22: number = Number(vuelta2.value);
+  let vuelta33: number = Number(vuelta3.value);
+  let vuelta44: number = Number(vuelta4.value);
+  console.log("el dato ingresado es ");
+});
